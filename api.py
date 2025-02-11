3337f9dddff181fb82fd2725219e1027e5202515
trickz <tiker.zone.teen@gmail.com>
2025-02-11 20:49:44 +0700

    success

--- api.py
+++ api.py
@@ -87,7 +87,7 @@ class query():
             cursor = conn.cursor(dictionary=True)
             cursor.execute(order)
             conn.commit()
-            
+
             id = cursor.lastrowid
             cursor.close()
             conn.close()
@@ -99,7 +99,6 @@ class query():
 
         except Exception as err:
             return {'msg': err, }
-    
 
     def put(order: String):
         try:
@@ -121,15 +120,6 @@ class query():
 # gettime ex. 2025-02-08 15:53:13
 
 class services():
-    def get_last_insert_id():
-        try:
-            result = query.get("SELECT last_insert_rowid() FROM ori")
-            return result[0]['last_insert_rowid()'] if result else None
-        except Exception as err:
-            print(f"Error getting last insert ID: {err}")
-            return None
-
-
 
     def get_date():
         now = datetime.now()
@@ -175,7 +165,14 @@ def register_user(item: register):
         try:
             res = query.post(
                 f"INSERT INTO user (username, email, password,create_date) VALUES ('{item.username}','{item.email}','{hash_pw}','{time_now}')")
-            return res
+            res_id = res['lastId']
+
+            res2 = query.post(f"INSERT INTO cart (user_id) VALUES ({res_id});")
+
+            return {"account": "accout has been added!",
+                    "cart": "cart has been added!",
+
+                    'msg': [res, res2]}
         except Exception as err:
             return {'msg': err,
                     'status': status}
@@ -188,141 +185,149 @@ def register_user(item: register):
 
 # login by get
 class loginform(BaseModel):
-    username:str
-    password:str
-    
+    username: str
+    password: str
+
+
 @app.post('/login.user')
-def user_login(data : loginform):
+def user_login(data: loginform):
     try:
         pw = services.hash(data.password)
-        
+
         conn = get_DB()
         cursor = conn.cursor(dictionary=True)
-        cursor.execute(f"SELECT * FROM user WHERE username = '{data.username}' AND password = '{pw}'")
+        cursor.execute(
+            f"SELECT * FROM user WHERE username = '{data.username}' AND password = '{pw}'")
         rows = cursor.fetchall()
         cursor.close()
         conn.close()
-        
+
         if not rows:
-            return {'msg': "user not found!",'status': 500,}
+            return {'msg': "user not found!", 'status': 500, }
         else:
             user_name = data.username
-            return {'user':user_name,'status':200,"body":rows}
+            return {'user': user_name, 'status': 200, "body": rows}
         # return res
     except Exception as err:
-            return {'msg': err}
-            # return {'msg': err,'status': status}
-            
-        
-        
+        return {'msg': err}
+        # return {'msg': err,'status': status}
+
+
 class addProducts (BaseModel):
-    products_name:str
-    products_desc:str
-    category_id:int
-    products_price:float
-    stock_quantity:int
-    img_url:str
+    products_name: str
+    products_desc: str
+    category_id: int
+    products_price: float
+    stock_quantity: int
+    img_url: str
+
+
 @app.post('/post.products/')
-def post_products(item:addProducts):
-    
+def post_products(item: addProducts):
+
     time = services.get_date()
     try:
-        res = query.post(f"INSERT INTO products (products_name,products_desc,products_price,stock_quantity,create_date,category_id) VALUES ('{item.products_name}','{item.products_desc}',{item.products_price},{item.stock_quantity},'{time}',{item.category_id})")
+        res = query.post(
+            f"INSERT INTO products (products_name,products_desc,products_price,stock_quantity,create_date,category_id) VALUES ('{item.products_name}','{item.products_desc}',{item.products_price},{item.stock_quantity},'{time}',{item.category_id})")
         res_id = res['lastId']
-        
-        
-        res2 = query.post(f"INSERT INTO img (img_url,products_id) VALUES ('{item.img_url}',{res_id})")     
-        
-        
-        print(res2)    
+
+        res2 = query.post(
+            f"INSERT INTO img (img_url,products_id) VALUES ('{item.img_url}',{res_id})")
+
+        print(res2)
         return {'products_msg': 'product has been added',
                 'img_mag': 'img has been added',
                 'status': 200
                 }
     except Exception as err:
-        return        
+        return
 
 
 @app.get('/get.{table}')
-def get_data(table:str):
-        try:
-            res = query.get(f"SELECT * FROM {table} WHERE del_frag = 'N'")
-            
-            return res
-        except Exception as err:
-            return {'msg': err,
-                    'status': 500}  
-             
+def get_data(table: str):
+    try:
+        res = query.get(f"SELECT * FROM {table} WHERE del_frag = 'N'")
+
+        return res
+    except Exception as err:
+        return {'msg': err,
+                'status': 500}
+
+
 @app.get('/get.{table}/{id}')
-def get_data(table:str,id:int):
-        try:
-            res = query.get(f"SELECT * FROM {table} WHERE del_frag = 'N' AND {table}_id = {id}")
-            
-            return res
-        except Exception as err:
-            return {'msg': err,
-                    'status': 500}   
-            
-            
-            
+def get_data(table: str, id: int):
+    try:
+        res = query.get(
+            f"SELECT * FROM {table} WHERE del_frag = 'N' AND {table}_id = {id}")
+
+        return res
+    except Exception as err:
+        return {'msg': err,
+                'status': 500}
+
+
 class sta_cat (BaseModel):
-    name:str
-    desc:str
-    
+    name: str
+    desc: str
+
+
 @app.post('/post.{table}')
-def post_category_status(table:str,data:sta_cat):
-    
+def post_category_status(table: str, data: sta_cat):
+
     try:
-        res = query.post(f"INSERT INTO {table} ({table}_name,{table}_desc) VALUES ('{data.name}','{data.desc}')")
+        res = query.post(
+            f"INSERT INTO {table} ({table}_name,{table}_desc) VALUES ('{data.name}','{data.desc}')")
         return res
     except Exception as err:
-        return  {'msg':err}  
-    
-    
-    
+        return {'msg': err}
+
+
 @app.put('/delete.{table}/{id}')
-def delete(table:str,id:int):
+def delete(table: str, id: int):
     try:
-        res = query.put(f"UPDATE {table} SET del_frag = 'Y' WHERE {table}_id = {id};")
+        res = query.put(
+            f"UPDATE {table} SET del_frag = 'Y' WHERE {table}_id = {id};")
         return res
     except Exception as err:
-        return  {'msg':err} 
-    
-    
+        return {'msg': err}
+
+
 @app.put('/delete2.img/{id}')
-def delete(id:int):
+def delete(id: int):
     try:
-        res = query.put(f"UPDATE img SET del_frag = 'Y' WHERE products_id = {id};")
+        res = query.put(
+            f"UPDATE img SET del_frag = 'Y' WHERE products_id = {id};")
         return res
     except Exception as err:
-        return  {'msg':err}
-     
+        return {'msg': err}
+
+
 @app.put('/delete2.products/{id}')
-def delete(id:int):
+def delete(id: int):
     try:
-        res = query.put(f"UPDATE products SET del_frag = 'Y' WHERE products_id = {id};")
+        res = query.put(
+            f"UPDATE products SET del_frag = 'Y' WHERE products_id = {id};")
         return res
     except Exception as err:
-        return  {'msg':err} 
-    
-    
-        
+        return {'msg': err}
+
+
 # @app.get('/get.product/')
 # def get_product():
 #     try:
-        
+
 #         res = query.get(f"SELECT * FROM products WHERE del_frag = 'N'")
 #         return {'msg': res}
 #     except Exception as err:
 #         return {'msg': err,
 #                 'status': 500}
-        
-        
+
+
 # # class product_img_id (BaseModel):
 # #     product : list
 # @app.get('/get.product.img/')
-# def get_product(): 
-#     try:      
+# def get_product():
+#     try:
 #         res = query.get(f"SELECT * FROM img WHERE del_frag = 'N'")
 #         return {'msg': res}
 #     except Exception as err:
@@ -330,36 +335,133 @@ def delete(id:int):
 #                 'status': 500}
 
 @app.get('/get_this_img/{id}')
-def get_img_id(id:int):
+def get_img_id(id: int):
     try:
-        
-        res = query.get(f"SELECT * FROM img WHERE del_frag = 'N' AND products_id = {id}")
+
+        res = query.get(
+            f"SELECT * FROM img WHERE del_frag = 'N' AND products_id = {id}")
         return res
     except Exception as err:
         return err
-    
+
+
 class editProducts (BaseModel):
-    products_id:int
-    products_name:str
-    products_desc:str
-    category_id:int
-    products_price:float
-    stock_quantity:int
-    img_url:str
+    products_id: int
+    products_name: str
+    products_desc: str
+    category_id: int
+    products_price: float
+    stock_quantity: int
+    img_url: str
+
+
 @app.put('/put.product_img')
-def put_pro_img(item:editProducts):
+def put_pro_img(item: editProducts):
     try:
         res = query.put(f"""UPDATE products SET products_name = '{item.products_name}',
                         products_desc = '{item.products_desc}',category_id = {item.category_id}, 
                         products_price = {item.products_price} , stock_quantity = {item.stock_quantity}
                         WHERE products_id = {item.products_id};""")
-         
+
         res2 = query.put(f"""
                          UPDATE img SET img_url = '{item.img_url}' WHERE products_id = {item.products_id}
                          """)
-        return {'product':res,
-                'img-product':res2
+        return {'product': res,
+                'img-product': res2
                 }
     except Exception as err:
         return
-    
+
+
+@app.get('/get_cart/{id}')
+def get_cartId_userId(id: int):
+    try:
+        res = query.get(f"SELECT * FROM cart WHERE user_id = {id}")
+        return {'msg': res}
+    except Exception as err:
+        return {'msg': err}
+
+
+@app.get('/get_cart_item/{id}')
+def get_cartId_userId(id: int):
+    try:
+        res = query.get(
+            f"SELECT * FROM cart_item WHERE cart_id = {id} AND del_frag ='N'")
+        return {'msg': res}
+    except Exception as err:
+        return {'msg': err}
+
+
+class cart(BaseModel):
+    products_id: int
+    cart_id: int
+    quantity: int
+
+
+@app.post('/cart_post.cartItem')
+def post_order_item(item: cart):
+    time = services.get_date()
+    try:
+        res = query.post(
+            f"INSERT INTO cart_item (cart_id,products_id,quantity,added_at) VALUES ({item.cart_id},{item.products_id},{item.quantity},'{time}');")
+
+        return {'msg': res}
+    except Exception as err:
+        return {'msg': err}
+
+
+@app.get('/get_cart_count/{id}')
+def get_cart_count(id: int):
+    try:
+        res = query.get(
+            f"SELECT COUNT(*) AS cart_items FROM cart_item WHERE cart_id = {id} AND del_frag = 'N'; ")
+
+        return {'msg': res}
+    except Exception as err:
+        return {'msg': err}
+
+
+class order(BaseModel):
+    products_data: list
+    user_id: int
+    amount_total: float
+
+
+@app.post('/order_order_item')
+def post_orders(item: order):
+    time = services.get_date()
+    status_id = 1
+
+    try:
+        res = query.post(f"INSERT INTO orders (user_id, order_date, status_id, total_amount) VALUES ({item.user_id},'{time}',{status_id},{item.amount_total})",)
+
+        res_id = res['lastId']
+
+        print(res_id)
+        # order Id Here
+
+        res_cart = []
+        res_order = []
+
+        for each in item.products_data:
+            cart_id = each['cart_item_id']
+            res_putCart = query.put(f"UPDATE cart_item SET del_frag = 'Y' WHERE cart_item_id = {cart_id}")
+
+            p_id = each['products_data']['products_id']
+            quantity = each['quantity']
+            price = each['products_data']['products_price']
+
+            res_orderItem = query.post(f"INSERT INTO order_item (order_id,products_id,quantity,price) VALUES ({res_id},{p_id},{quantity},{price})")
+
+            res_cart.append(res_putCart)
+            res_order.append(res_orderItem)
+
+        return {'Add order': res,
+                'change state': res_cart,
+                'orderItem': res_order}
+
+        return {'Add order': 'test',
+                }
+
+    except Exception as err:
+        return {'msg': err}
